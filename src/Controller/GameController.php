1e86e75d0cb2115ce591d5ca53118d33a759cc97
RayanDev23 <rayanquessada.rq@gmail.com>
2024-02-16 10:38:15 +0100

    Update GameController.php

--- src/Controller/GameController.php
+++ src/Controller/GameController.php
@@ -18,10 +18,34 @@ class GameController extends AbstractController
         }
         return $this->json($data);
     }
-    
+
     #[Route('/game/{id}', name: 'game_by_id', methods:"GET")]
     public function getGameById(Game $game):JsonResponse
     {
         return $this->json(['id' => $game->getId()]);
+    }
+
+    #[Route('/game/', name: 'game_create', methods:"POST")]
+    public function createGame (Request $request):JsonResponse
+    {
+        $data = json_decode($request->getContent(), true);
+
+        $game = new Game();
+
+        $entityManager = $this->getDoctrine()->getManager();
+        $entityManager->persist($game);
+        $entityManager->flush();
+
+        return $this->json(['message' => 'Game created successfully', 'id' => $game->getId()]);
+    }
+
+    #[Route('/game/{id}', name: 'game_delete', methods:"DELETE")]
+    public function deleteGame(Game $game):JsonResponse
+    {
+        $entityManager = $this->getDoctrine()->getManager();
+        $entityManager->remove($game);
+        $entityManager->flush();
+
+        return $this->json(['message' => 'Game deleted successfully']);
     }
 }
