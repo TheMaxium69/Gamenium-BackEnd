6407c64f5a4d97d04d1367505d073c75bd654675
RayanDev23 <rayanquessada.rq@gmail.com>
2024-02-16 10:24:38 +0100

    Update GameController.php

--- src/Controller/GameController.php
+++ src/Controller/GameController.php
@@ -9,14 +9,19 @@ use Symfony\Component\Routing\Annotation\Route;
 class GameController extends AbstractController
 {
     #[Route('/games/', name: 'game_all', methods:"GET")]
-    public function getGameAll(Game $game):JsonResponse
+    public function getGameAll():JsonResponse
     {
-        return $this->json(['id' => $game->getId()]);
+        $games = $this->getDoctrine()->getRepository(Game::class)->findAll();
+        $data = [];
+        foreach ($games as $game){
+            $data[] = ['id' => $game->getId()];
+        }
+        return $this->json($data);
     }
+    
     #[Route('/game/{id}', name: 'game_by_id', methods:"GET")]
     public function getGameById(Game $game):JsonResponse
     {
         return $this->json(['id' => $game->getId()]);
-
     }
 }
