4d45117dffd18467a7c124c4a39d6bd053bf2969
RayanDev23 <rayanquessada.rq@gmail.com>
2024-02-16 10:04:30 +0100

    Entity Game creation

---
+++ src/Controller/GameController.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class GameController extends AbstractController
+{
+    #[Route('/game', name: 'app_game')]
+    public function index(): Response
+    {
+        return $this->render('game/index.html.twig', [
+            'controller_name' => 'GameController',
+        ]);
+    }
+
+
+    public function getGameDetails(Game $game):JsonResponse{
+        
+    }
+}
