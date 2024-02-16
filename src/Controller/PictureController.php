9f51c66f8547ded9952811639309ef9f98c59740
Threonaum <lepolak01@gmail.com>
2024-02-16 15:16:15 +0100

    Désolé la madame de maxime

--- src/Controller/PictureController.php
+++ src/Controller/PictureController.php
@@ -11,7 +11,7 @@ use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;   
 
-class GameController extends AbstractController
+class PictureController extends AbstractController
 {
 
     private $manager;
