28032da90d4ac76d02f8ade9d312232d5be3875e
RayanDev23 <rayanquessada.rq@gmail.com>
2024-02-20 10:45:25 +0100

    Update CommentController.php

--- src/Controller/CommentController.php
+++ src/Controller/CommentController.php
@@ -2,6 +2,12 @@
 
 namespace App\Controller;
 
+use App\Entity\Comment;
+use App\Repository\CommentRepository;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\HttpFoundation\JsonResponse;
+
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
