8a31ad9d9882a52c583784170fbf5c6dfca5d72e
AngeloNix64 <angelofernandez15@outlook.fr>
2024-02-23 14:08:06 +0100

    Entity game

---
+++ src/Entity/Game.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\GameRepository;
+use Doctrine\DBAL\Types\Types;
+use Doctrine\ORM\Mapping as ORM;
+
+
+#[ORM\Entity(repositoryClass: GameRepository::class)]
+class Game
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+}
