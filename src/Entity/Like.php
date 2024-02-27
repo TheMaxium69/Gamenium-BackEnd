b6f22b8580eb70d226c38898e1f4f7edb705a657
RayanDev23 <rayanquessada.rq@gmail.com>
2024-02-27 10:11:24 +0100

    Update Like.php

--- src/Entity/Like.php
+++ src/Entity/Like.php
@@ -20,14 +20,15 @@ class Like
     #[ORM\Column]
     private ?int $idComment = null;
 
-    #[ORM\Column]
-    private ?int $idUser = null;
 
     #[ORM\Column(length: 255)]
     private ?string $ip = null;
 
     #[ORM\Column]
     private ?\DateTimeImmutable $createdAt = null;
+
+    #[ORM\ManyToOne]
+    private ?user $user = null;
 
     public function getId(): ?int
     {
@@ -58,18 +59,6 @@ class Like
         return $this;
     }
 
-    public function getIdUser(): ?int
-    {
-        return $this->idUser;
-    }
-
-    public function setIdUser(int $idUser): static
-    {
-        $this->idUser = $idUser;
-
-        return $this;
-    }
-
     public function getIp(): ?string
     {
         return $this->ip;
@@ -90,6 +79,18 @@ class Like
     public function setCreatedAt(\DateTimeImmutable $createdAt): static
     {
         $this->createdAt = $createdAt;
+
+        return $this;
+    }
+
+    public function getUser(): ?user
+    {
+        return $this->user;
+    }
+
+    public function setUser(?user $user): static
+    {
+        $this->user = $user;
 
         return $this;
     }
