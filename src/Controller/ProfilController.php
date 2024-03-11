bad57865ac8c954bea743d4fd280ca8cbb732e8d
Maxime Tournier <mxm.tournier@gmail.com>
2024-03-11 16:33:50 +0100

    Update ProfilController.php

--- src/Controller/ProfilController.php
+++ src/Controller/ProfilController.php
@@ -25,6 +25,37 @@ class ProfilController extends AbstractController
 
         } else {
 
+            if ($user->getId() == 2){
+                $reseau = [
+                    [
+                        "url" => "https://instagram.com/the_maxime_san",
+                        "serice" => [
+                            "id"=>0,
+                            "name"=>"instagram",
+                            "icon_class"=>"ri-instagram-line"
+                        ]
+                    ],
+                    [
+                        "url" => "https://github.com/TheMaxium69",
+                        "serice" => [
+                            "id"=>1,
+                            "name"=>"github",
+                            "icon_class"=>"ri-github-fill"
+                        ]
+                    ],
+                    [
+                        "url" => "https://linkedin.com/in/maxime-tournier-tyrolium",
+                        "serice" => [
+                            "id"=>2,
+                            "name"=>"linkedin",
+                            "icon_class"=>"ri-linkedin-box-fill"
+                        ]
+                    ],
+                ];
+            } else {
+                $reseau = null;
+            }
+
             $message = [
                 'message' => "good",
                 'result' => [
@@ -37,11 +68,7 @@ class ProfilController extends AbstractController
                     "picture" => $user->getPp()->getUrl(),
                     "nbGame" => 10,
                     "nbNote" => 5,
-                    "reseau" => [
-                        "steam" => "url",
-                        "twich" => "url",
-                        "instagram" => "url"
-                    ]
+                    "reseau" => $reseau
                 ]
             ];
 
