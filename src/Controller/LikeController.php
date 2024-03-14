6c9f856681c6d97bc1dc908dbead0b9233b7137f
RayanDev23 <rayanquessada.rq@gmail.com>
2024-03-14 13:57:37 +0100

    Update LikeController.php

--- src/Controller/LikeController.php
+++ src/Controller/LikeController.php
@@ -4,6 +4,8 @@ namespace App\Controller;
 
 use App\Entity\Like;
 use App\Repository\LikeRepository;
+use App\Repository\UserRepository;
+use App\Repository\CommentRepository;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
@@ -11,11 +13,14 @@ use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
+
 class LikeController extends AbstractController
 {
     public function __construct(
         private EntityManagerInterface $entityManager,
-        private LikeRepository $likeRepository
+        private LikeRepository $likeRepository,
+        private CommentRepository $commentRepository,
+        private UserRepository $userRepository
     ) {}
 
     #[Route('/likes', name: 'get_all_likes', methods: ['GET'])]
@@ -125,4 +130,50 @@ class LikeController extends AbstractController
 
         return $this->json($response);
     }
+
+
+    #[Route('/like/comment/{idComment}', name: 'get_likes_count_by_comment', methods: ['GET'])]
+    public function LikesCountByComment(int $idComment): JsonResponse
+    {
+        $likesCount = $this->likeRepository->count(['comment' => $idComment]);
+
+        $likedUsers = $this->likeRepository->findBy(['comment' => $idComment]);
+
+        $response = [
+            'message' => 'good',
+            'total' => $likesCount,
+            'result' => array_map(fn($like) => ['id' => $like->getUser()->getId()], $likedUsers)
+        ];
+
+        return $this->json($response);
+    }
+
+
+
+    #[Route('/like/comment/{idComment}', name: 'add_like_to_comment', methods: ['POST'])]
+public function addLikeToComment(int $idComment, Request $request): JsonResponse
+{
+    $data = json_decode($request->getContent(), true);
+
+    if (!isset($data['user_id'])) {
+        return $this->json(['error' => 'Les données fournies sont incomplètes'], Response::HTTP_BAD_REQUEST);
+    }
+
+    $comment = $this->commentRepository->find($idComment);
+
+    if (!$comment) {
+        return $this->json(['error' => 'Commentaire non trouvé'], Response::HTTP_NOT_FOUND);
+    }
+
+    $like = new Like();
+    $like->setUser($this->userRepository->find($data['user_id']));
+    $like->setComment($comment);
+    $like->setIp($data['ip']);
+    $like->setCreatedAt(new \DateTimeImmutable());
+
+    $this->entityManager->persist($like);
+    $this->entityManager->flush();
+
+    return $this->json(['message' => 'Like ajouté avec succès au commentaire'], Response::HTTP_CREATED);
+}
     }
