79ee9a29c57fbe0de90c3ec272f3ad1d198ee152
AngeloNix64 <angelofernandez15@outlook.fr>
2024-03-27 16:57:17 +0100

    Update LikeController.php

--- src/Controller/LikeController.php
+++ src/Controller/LikeController.php
@@ -5,6 +5,7 @@ namespace App\Controller;
 use App\Entity\Like;
 use App\Repository\LikeRepository;
 use App\Repository\UserRepository;
+use App\Repository\PostActuRepository;
 use App\Repository\CommentRepository;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
@@ -20,7 +21,8 @@ class LikeController extends AbstractController
         private EntityManagerInterface $entityManager,
         private LikeRepository $likeRepository,
         private CommentRepository $commentRepository,
-        private UserRepository $userRepository
+        private UserRepository $userRepository,
+        private PostActuRepository $postActuRepository
     ) {}
 
     #[Route('/likes', name: 'get_all_likes', methods: ['GET'])]
@@ -31,39 +33,71 @@ class LikeController extends AbstractController
         return $this->json($likes , 200 , [], ['groups' => 'like:read']);
     }
 
-    #[Route('/like/post-actu/{idPost}', name: 'get_likes_count', methods: ['GET'])]
-    public function getLikesCount(int $idPost): JsonResponse
+    #[Route('/like/post-actu/{idPost}', name: 'get_post_actu_likes', methods: ['GET'])]
+    public function getPostActuLikes(int $idPost): JsonResponse
     {
 
-        $likesCount = $this->likeRepository->count(['post' => $idPost]);
+        $postActu = $this->postActuRepository->find($idPost);
 
 
-        $likedUsers = $this->likeRepository->findBy(['post' => $idPost]);
+        if (!$postActu) {
+            return $this->json(['error' => 'Post actualité non trouvé']);
+        }
 
-        $response = [
-            'message' => 'good',
-            'total' => $likesCount,
-            'result' => array_map(fn($like) => ['id' => $like->getUser()->getId()], $likedUsers)
-        ];
 
-        // Retourner la réponse JSON
-        return $this->json($response);
+        $postActuLikes = $this->likeRepository->findBy(['post' => $postActu]);
+
+
+        if (empty($postActuLikes)) {
+            return $this->json(['message' => 'Aucun like trouvé pour ce post actualité']);
+        }
+
+
+        $likesInfo = [];
+        foreach ($postActuLikes as $like) {
+            $likeInfo = [
+                'id' => $like->getId(),
+                'user_id' => $like->getUser()->getId(),
+                'ip' => $like->getIp(),
+                'created_at' => $like->getCreatedAt()->format('Y-m-d H:i:s'),
+            ];
+            $likesInfo[] = $likeInfo;
+        }
+
+        return $this->json($likesInfo);
     }
 
-    #[Route('/like/comment/{idComment}', name: 'get_likes_count_by_comment', methods: ['GET'])]
-    public function getLikesCountByComment(int $idComment): JsonResponse
+
+    #[Route('/like/comment/{idComment}', name: 'get_comment_likes', methods: ['GET'])]
+    public function getCommentLikes(int $idComment): JsonResponse
     {
-        $likesCount = $this->likeRepository->count(['comment' => $idComment]);
 
-        $likedUsers = $this->likeRepository->findBy(['comment' => $idComment]);
+        $comment = $this->commentRepository->find($idComment);
 
-        $response = [
-            'message' => 'good',
-            'total' => $likesCount,
-            'result' => array_map(fn($like) => ['id' => $like->getUser()->getId()], $likedUsers)
-        ];
+        if (!$comment) {
+            return $this->json(['error' => 'Commentaire non trouvé']);
+        }
 
-        return $this->json($response);
+
+        $commentLikes = $this->likeRepository->findBy(['comment' => $comment]);
+
+        if (empty($commentLikes)) {
+            return $this->json(['message' => 'Aucun like trouvé pour ce commentaire']);
+        }
+
+
+        $likesInfo = [];
+        foreach ($commentLikes as $like) {
+            $likeInfo = [
+                'id' => $like->getId(),
+                'user_id' => $like->getUser()->getId(),
+                'ip' => $like->getIp(),
+                'created_at' => $like->getCreatedAt()->format('Y-m-d H:i:s'),
+            ];
+            $likesInfo[] = $likeInfo;
+        }
+
+        return $this->json($likesInfo);
     }
 
     #[Route('/like/{id}', name: 'get_like_by_id', methods: ['GET'])]
@@ -72,108 +106,134 @@ class LikeController extends AbstractController
         $like = $this->likeRepository->find($id);
 
         if (!$like) {
-            return $this->json(['message' => 'like not found'], Response::HTTP_NOT_FOUND);
+            return $this->json(['message' => 'like not found']);
         }
 
         return $this->json($like);
     }
 
-    #[Route('/like', name: 'create_like', methods: ['POST'])]
-    public function createlike(Request $request): JsonResponse
-    {
-        $data = json_decode($request->getContent(), true);
-
-        $like = new Like();
-        $like->setIdPost($data['idPost']);
-        $like->setIdComment($data['idComment']);
-        $like->setIdUser($data['idUser']);
-        $like->setIp($data['ip']);
-        $like->setCreatedAt(new \DateTimeImmutable());
+        #[Route('/like', name: 'create_like', methods: ['POST'])]
+        public function createLike2(Request $request): JsonResponse
+        {
+            $data = json_decode($request->getContent(), true);
 
 
-        $this->entityManager->persist($like);
-        $this->entityManager->flush();
+            if (!isset($data['user_id']) || !isset($data['ip']) || (!isset($data['post_id']) && !isset($data['comment_id']))) {
+                return $this->json(['error' => 'Les données fournies sont incomplètes'], Response::HTTP_BAD_REQUEST);
+            }
 
-        return $this->json(['message' => 'like created successfully'], Response::HTTP_CREATED);
+
+            $like = new Like();
+
+
+            $user = $this->userRepository->find($data['user_id']);
+            if (!$user) {
+                return $this->json(['error' => 'Utilisateur non trouvé']);
+            }
+
+
+            $like->setUser($user);
+            $like->setIp($data['ip']);
+            $like->setCreatedAt(new \DateTimeImmutable());
+
+
+            if (isset($data['post_id'])) {
+
+                $post = $this->postActuRepository->find($data['post_id']);
+                if (!$post) {
+                    return $this->json(['error' => 'Post non trouvé']);
+                }
+
+                $like->setPost($post);
+            } elseif (isset($data['comment_id'])) {
+
+                $comment = $this->commentRepository->find($data['comment_id']);
+                if (!$comment) {
+                    return $this->json(['error' => 'Commentaire non trouvé']);
+                }
+
+
+                $like->setComment($comment);
+            }
+
+
+            $this->entityManager->persist($like);
+            $this->entityManager->flush();
+
+        return $this->json(['message' => 'Like créé avec succès'], Response::HTTP_CREATED);
     }
 
-    #[Route('/like/{id}', name: 'delete_like', methods: ['DELETE'])]
-    public function deletelike(int $id): JsonResponse
+    #[Route('/like/post-actu/{postId}', name: 'delete_post_actu_like', methods: ['DELETE'])]
+    public function deletePostActuLike(int $postId, Request $request): JsonResponse
     {
-        $like = $this->likeRepository->find($id);
+
+        $data = json_decode($request->getContent(), true);
+
+
+        $user = $this->userRepository->find($data['user_id']);
+
+
+        if (!$user) {
+            return $this->json(['error' => 'Utilisateur non authentifié'], Response::HTTP_UNAUTHORIZED);
+        }
+
+
+        $postActu = $this->postActuRepository->find($postId);
+
+
+        if (!$postActu) {
+            return $this->json(['error' => 'Post actualité non trouvé']);
+        }
+
+
+        $like = $this->likeRepository->findOneBy(['post' => $postActu, 'user' => $user]);
+
 
         if (!$like) {
-            return $this->json(['message' => 'like not found'], Response::HTTP_NOT_FOUND);
+            return $this->json(['error' => 'Like non trouvé pour ce post actualité']);
+        }
+
+
+        $this->entityManager->remove($like);
+        $this->entityManager->flush();
+
+        return $this->json(['message' => 'Like sur le post actualité supprimé avec succès']);
+    }
+
+    #[Route('/like/comment/{commentId}', name: 'delete_comment_like', methods: ['DELETE'])]
+    public function deleteCommentLike(int $commentId, Request $request): JsonResponse
+    {
+
+        $data = json_decode($request->getContent(), true);
+
+
+        $user = $this->userRepository->find($data['user_id']);
+
+
+        if (!$user) {
+            return $this->json(['error' => 'Utilisateur non authentifié'], Response::HTTP_UNAUTHORIZED);
+        }
+
+        $comment = $this->commentRepository->find($commentId);
+
+
+        if (!$comment) {
+            return $this->json(['error' => 'Commentaire non trouvé']);
+        }
+
+
+        $like = $this->likeRepository->findOneBy(['comment' => $comment, 'user' => $user]);
+
+
+        if (!$like) {
+            return $this->json(['error' => 'Like non trouvé pour ce commentaire']);
         }
 
         $this->entityManager->remove($like);
         $this->entityManager->flush();
 
-        return $this->json(['message' => 'like deleted successfully']);
+        return $this->json(['message' => 'Like sur le commentaire supprimé avec succès']);
     }
 
 
-
-
-    #[Route('/like/post-actu/{idPost}', name: 'get_likes_count', methods: ['GET'])]
-    public function LikesCount(int $idPost): JsonResponse
-    {
-        $likesCount = $this->likeRepository->count(['post' => $idPost]);
-
-        $likedUsers = $this->likeRepository->findBy(['post' => $idPost]);
-
-        $response = [
-            'message' => 'good',
-            'total' => $likesCount,
-            'result' => array_map(fn($like) => ['id' => $like->getUser()->getId()], $likedUsers)
-        ];
-
-        return $this->json($response);
-    }
-
-
-    #[Route('/like/comment/{idComment}', name: 'get_likes_count_by_comment', methods: ['GET'])]
-    public function LikesCountByComment(int $idComment): JsonResponse
-    {
-        $likesCount = $this->likeRepository->count(['comment' => $idComment]);
-
-        $likedUsers = $this->likeRepository->findBy(['comment' => $idComment]);
-
-        $response = [
-            'message' => 'good',
-            'total' => $likesCount,
-            'result' => array_map(fn($like) => ['id' => $like->getUser()->getId()], $likedUsers)
-        ];
-
-        return $this->json($response);
-    }
-
-
-
-    #[Route('/like/comment/{idComment}', name: 'add_like_to_comment', methods: ['POST'])]
-public function addLikeToComment(int $idComment, Request $request): JsonResponse
-{
-    $data = json_decode($request->getContent(), true);
-
-    if (!isset($data['user_id'])) {
-        return $this->json(['error' => 'Les données fournies sont incomplètes'], Response::HTTP_BAD_REQUEST);
-    }
-
-    $comment = $this->commentRepository->find($idComment);
-
-    if (!$comment) {
-        return $this->json(['error' => 'Commentaire non trouvé'], Response::HTTP_NOT_FOUND);
-    }
-
-    $like = new Like();
-    $like->setUser($this->userRepository->find($data['user_id']));
-    $like->setComment($comment);
-    $like->setIp($data['ip']);
-    $like->setCreatedAt(new \DateTimeImmutable());
-
-    $this->entityManager->persist($like);
-    $this->entityManager->flush();
-
-    return $this->json(['message' => 'Like ajouté avec succès au commentaire'], Response::HTTP_CREATED);
 }
-    }
