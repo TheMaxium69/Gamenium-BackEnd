5429638fe6d6194cade0aa5002d49cbfc20b5e60
TheMaxium69 <mxm.tournier@gmail.com>
2024-11-27 14:22:01 +0100

    Final delete
    Co-Authored-By: Oscar <159458439+05c4rR@users.noreply.github.com>

--- src/Controller/HistoryMyGameController.php
+++ src/Controller/HistoryMyGameController.php
@@ -249,12 +249,15 @@ class HistoryMyGameController extends AbstractController
 
     #[Route('/deleteMyGame/{id}', name:'deleteMyGame', methods:['DELETE'])]
     public function deleteMyGame(Request $request, int $id) : JsonResponse 
-    {   
-        $data = json_decode($request->getContent(), true);
+    {
 
-        /*SI LE JSON A PAS DE SOUCI */
-        if ($data === null && json_last_error() !== JSON_ERROR_NONE) {
-            return $this->json(['message' => 'Invalid JSON format']);
+        if (!$id) {
+            return $this->json(['message' => 'id is required']);
+        }
+
+        $myGame = $this->entityManager->getRepository(HistoryMyGame::class)->findOneBy(['id' => $id]);
+        if (!$myGame) {
+            return $this->json(['message' => 'Jeu introuvable']);
         }
 
         $authorizationHeader = $request->headers->get('Authorization');
@@ -268,35 +271,26 @@ class HistoryMyGameController extends AbstractController
                 return $this->json(['message' => 'token is failed']);
             }
 
-            // Trouve l'entrée dans la table hystory_my_game
-            $myGame = $this->entityManager->getRepository(HistoryMyGame::class)->findOneBy(
-                [
-                    'id' => $id,
-                    'user' => $user
-                ]
-            );
-
-            if (!$myGame) {
-                return $this->json(['message' => 'Jeu introuvable']);
+            // Verifi si il est bien a lui
+            if ($myGame->getUser() != $user){
+                return $this->json(['message' => 'no permission']);
             }
+
             
             // Trouve l'entrée dans hmg_copy correspondante
-            $myGameCopy = $this->entityManager->getRepository(HmgCopy::class)->findOneBy(
-                [
-                    'HistoryMyGame' => $myGame
-                ]
-            );
-            
-            if (!$myGameCopy) {
-                return $this->json(['message' => 'Copy du jeu introuvable']);
+            $myGameCopy = $this->entityManager->getRepository(HmgCopy::class)->findBy(['HistoryMyGame' => $myGame]);
+
+            /* SI YA PAS DE COPY C PAS GRAVE*/
+            if ($myGameCopy) {
+                foreach ($myGameCopy as $oneMyGameCopy) {
+                    $this->entityManager->remove($oneMyGameCopy);
+                }
             }
 
-            // Suppresion des jeux dans history_my_game et hmg_copy
-            $this->entityManager->remove($myGameCopy);
             $this->entityManager->remove($myGame);
             $this->entityManager->flush();
 
-            return $this->json(['message' => 'delete success']);    
+            return $this->json(['message' => 'delete success']);
             
         }
 
