1e1acc775bd65168244e4432c1e8e97a91a9409a
TheMaxium69 <mxm.tournier@gmail.com>
2024-12-04 16:39:52 +0100

    view provider final

--- src/Controller/ViewController.php
+++ src/Controller/ViewController.php
@@ -3,10 +3,12 @@
 namespace App\Controller;
 
 use App\Entity\PostActu;
+use App\Entity\Provider;
 use App\Entity\User;
 use App\Entity\View;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -44,6 +46,12 @@ class ViewController extends AbstractController
 
         $idPostActu = $data['id'];
 
+        /*SI L'ACTU EXISTE*/
+        $postActu = $this->entityManager->getRepository(PostActu::class)->findOneBy(['id' => $idPostActu]);
+        if (!$postActu) {
+            return $this->json(['message' => 'actu is failed']);
+        }
+
         $authorizationHeader = $request->headers->get('Authorization');
 
         /*SI LE TOKEN EST REMPLIE */
@@ -53,12 +61,6 @@ class ViewController extends AbstractController
             /*SI LE TOKEN A BIEN UN UTILISATEUR EXITANT - SINON C PAS GRAVE SA SERA ANNONYME */
             $user = $this->entityManager->getRepository(User::class)->findOneBy(['token' => $token]);
 
-        }
-
-        /*SI L'ACTU EXISTE*/
-        $postActu = $this->entityManager->getRepository(PostActu::class)->findOneBy(['id' => $idPostActu]);
-        if (!$postActu) {
-            return $this->json(['message' => 'actu is failed']);
         }
 
         $view = new View();
@@ -73,6 +75,63 @@ class ViewController extends AbstractController
         $this->entityManager->flush();
 
         return $this->json(['message' => 'good', 'result' => $view], 200, [], ['groups' => 'view:read']);
+
+    }
+
+    #[Route('-provider-add', name: 'app_view_provider_add', methods: ['POST'])]
+    public function addProviderView(Request $request): JsonResponse
+    {
+
+        $data = json_decode($request->getContent(), true);
+
+        /*SI LE JSON A PAS DE SOUCI */
+        if ($data === null && json_last_error() !== JSON_ERROR_NONE) {
+            return $this->json(['message' => 'Invalid JSON format']);
+        }
+
+        /*SI LES CHAMP SON REMPLIE */
+        if (!isset($data['id'])){
+            return $this->json(['message' => 'undefine of field']);
+        }
+
+        /* SET UNE IP */
+        if (!isset($data['ip'])) {
+            $newIp = "0.0.0.0";
+        } else {
+            $newIp = $data['ip'];
+        }
+
+        $idProvider = $data['id'];
+
+        /*SI LE PROVIDER EXISTE*/
+        $provider = $this->entityManager->getRepository(Provider::class)->findOneBy(['id' => $idProvider]);
+        if (!$provider){
+            return $this->json(['message' => 'provider is failed']);
+        }
+
+        $authorizationHeader = $request->headers->get('Authorization');
+
+        if (strpos($authorizationHeader, 'Bearer ') === 0) {
+            $token = substr($authorizationHeader, 7);
+
+            /*SI LE TOKEN A BIEN UN UTILISATEUR EXITANT - SINON C PAS GRAVE SA SERA ANNONYME */
+            $user = $this->entityManager->getRepository(User::class)->findOneBy(['token' => $token]);
+
+        }
+
+        $view = new View();
+        $view->setProvider($provider);
+        if ($user) {
+            $view->setWho($user);
+        }
+        $view->setIp($newIp);
+        $view->setViewAt(new \DateTimeImmutable());
+
+        $this->entityManager->persist($view);
+        $this->entityManager->flush();
+
+        return $this->json(['message' => 'good', 'result' => $view], 200, [], ['groups' => 'view:read']);
+
 
     }
 }
