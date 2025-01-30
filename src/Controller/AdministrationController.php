e519c5824ed77e624800d916e4fc785cd16023fe
TheMaxium69 <mxm.tournier@gmail.com>
2025-01-30 14:30:55 +0100

    update

--- src/Controller/AdministrationController.php
+++ src/Controller/AdministrationController.php
@@ -2,24 +2,57 @@
 
 namespace App\Controller;
 
+use App\Repository\UserRepository;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 #[Route('admin')]
 class AdministrationController extends AbstractController
 {
+
+    private $manager;
+    private $user;
+    private UserRepository $userRepository;
+
+    public function __construct(EntityManagerInterface $manager, UserRepository $user, UserRepository $userRepository)
+    {
+        $this->manager = $manager;
+        $this->user = $user;
+        $this->userRepository = $userRepository;
+    }
+
+
     #[Route('-test', name: 'app_administration')]
     public function test(): JsonResponse
     {
 
-
-
-
-
-
         return $this->json(['message' => 'good']);
 
+    }
+
+
+
+
+    #[Route('-users-search', name: 'search_users_admin', methods: ['POST'])]
+    public function searchUsers(Request $request): JsonResponse
+    {
+        $data = json_decode($request->getContent(), true);
+        $searchValue = $data['searchValue'] ?? '';
+        $limit = $data['limit'];
+
+        $results = $this->userRepository->searchUserByName($searchValue, $limit);
+
+        $finalResults = [];
+        foreach ($results as $user) {
+            if (!in_array('ROLE_BAN', $user->getRoles())) {
+                $finalResults[] = $user;
+            }
+        }
+
+        return $this->json($finalResults, 200, [], ['groups' => 'useradmin:read']);
     }
 }
--- src/Entity/User.php
+++ src/Entity/User.php
@@ -16,51 +16,53 @@ class User implements UserInterface
     #[ORM\Id]
     #[ORM\GeneratedValue]
     #[ORM\Column]
-    #[Groups(['comment:read', 'screenshot:read' , 'historygame:read' , 'like:read', 'picture:read', 'post:read', 'userRate:read', 'badgesversuser:read', 'user:read', 'followProvider:read', 'followPageGame:read', 'follow:read', 'taskusercompleted:read', 'view:read', 'commentreply:read', 'default:read', 'historyplateform:read', 'warn:read'])]
+    #[Groups(['useradmin:read', 'comment:read', 'screenshot:read' , 'historygame:read' , 'like:read', 'picture:read', 'post:read', 'userRate:read', 'badgesversuser:read', 'user:read', 'followProvider:read', 'followPageGame:read', 'follow:read', 'taskusercompleted:read', 'view:read', 'commentreply:read', 'default:read', 'historyplateform:read', 'warn:read'])]
     private ?int $id = null;
 
     #[ORM\Column]
-    #[Groups(['user:read', 'comment:read', 'commentreply:read'])]
+    #[Groups(['useradmin:read', 'user:read', 'comment:read', 'commentreply:read'])]
     private ?int $id_useritium = null;
 
     #[ORM\Column(type: 'json')]
     private array $roles = [];
 
     #[ORM\Column]
-    #[Groups(['user:read', 'comment:read', 'commentreply:read'])]
+    #[Groups(['useradmin:read', 'user:read', 'comment:read', 'commentreply:read'])]
     private ?\DateTimeImmutable $joinAt = null;
 
     #[ORM\Column]
-    #[Groups(['user:read'])]
+    #[Groups(['useradmin:read', 'user:read'])]
     private ?\DateTimeImmutable $lastConnection = null;
 
     #[ORM\Column]
+    #[Groups(['useradmin:read'])]
     private array $ip = [];
 
     #[ORM\Column(length: 255)]
+    #[Groups(['useradmin:read'])]
     private ?string $email = null;
 
     #[ORM\Column(length: 255, nullable: true)]
-    #[Groups(['user:read', 'comment:read', 'commentreply:read'])]
+    #[Groups(['useradmin:read', 'user:read', 'comment:read', 'commentreply:read'])]
     private ?string $displayname_useritium = null;
 
     #[ORM\Column(length: 255)]
-    #[Groups(['default:read', 'screenshot:read', 'user:read', 'comment:read', 'historygame:read','taskusercompleted:read', 'view:read', 'commentreply:read', 'historyplateform:read'])]
+    #[Groups(['useradmin:read', 'default:read', 'screenshot:read', 'user:read', 'comment:read', 'historygame:read','taskusercompleted:read', 'view:read', 'commentreply:read', 'historyplateform:read'])]
     private ?string $username = null;
 
     #[ORM\Column(length: 255)]
-    #[Groups(['user:read','comment:read', 'commentreply:read', 'warn:read'])]
+    #[Groups(['useradmin:read', 'user:read','comment:read', 'commentreply:read', 'warn:read'])]
     private ?string $displayname = null;
 
     #[ORM\Column(type: Types::TEXT)]
     private ?string $token = null;
 
     #[ORM\ManyToOne(cascade: ['persist'], targetEntity: Picture::class)]
-    #[Groups(['user:read', 'comment:read', 'commentreply:read'])]
+    #[Groups(['useradmin:read', 'user:read', 'comment:read', 'commentreply:read'])]
     private ?picture $pp = null;
 
     #[ORM\Column(length: 255, nullable: true)]
-    #[Groups(['user:read', 'comment:read', 'commentreply:read'])]
+    #[Groups(['useradmin:read', 'user:read', 'comment:read', 'commentreply:read'])]
     private ?string $color = null;
 
     #[ORM\OneToMany(targetEntity: Log::class, mappedBy: 'user', orphanRemoval: true)]
