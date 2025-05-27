d6e7bd87c291309ed9c909d62295dc393d6d6daf
TheMaximeSan <mxm.tournier@gmail.com>
2025-05-27 15:40:09 +0200

    update

--- src/Controller/ProviderController.php
+++ src/Controller/ProviderController.php
@@ -18,7 +18,7 @@ class ProviderController extends AbstractController
         private ProviderRepository $providerRepository
         ) {}
 
-    #[Route('/provider', name: 'provider_all', methods:'GET')]
+    #[Route('/providers', name: 'provider_all', methods:'GET')]
     public function getProviderAll(): JsonResponse
     {
         $providers = $this->providerRepository->findAll();
